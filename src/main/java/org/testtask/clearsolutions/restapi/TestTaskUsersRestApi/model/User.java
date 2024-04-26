a00c044096d471e1a6d020c972b648e320c429f4
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2024-04-26 15:17:42 +0300

    Added @Builder annotation

--- src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/model/User.java
+++ src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/model/User.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
+@Builder
 public class User {
 
     @NotBlank
