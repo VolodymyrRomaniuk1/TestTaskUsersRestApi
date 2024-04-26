2a8c7f009f7446370d5e3040b2f134b67993c801
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2024-04-26 15:16:30 +0300

    Added custom UserNotFoundException

---
+++ src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/exception/UserNotFoundException.java
@@ -0,0 +1,19 @@
+package org.testtask.clearsolutions.restapi.TestTaskUsersRestApi.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
