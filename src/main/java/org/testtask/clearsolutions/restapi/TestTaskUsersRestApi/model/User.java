996523a0c74250c0088ed13c7feaceb87c7d8346
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2024-04-26 12:46:46 +0300

    Added User model class

---
+++ src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/model/User.java
@@ -0,0 +1,31 @@
+package org.testtask.clearsolutions.restapi.TestTaskUsersRestApi.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotNull
+    @Past
+    private LocalDate birthDate;
+
+    private String address;
+
+    private String phoneNumber;
+}
