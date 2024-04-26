2c55b65ca64ef702792bceaeeed21af08529661b
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2024-04-26 12:09:49 +0300

    Initial commit

---
+++ src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/TestTaskUsersRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.testtask.clearsolutions.restapi.TestTaskUsersRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskUsersRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskUsersRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/TestTaskUsersRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testtask.clearsolutions.restapi.TestTaskUsersRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskUsersRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
