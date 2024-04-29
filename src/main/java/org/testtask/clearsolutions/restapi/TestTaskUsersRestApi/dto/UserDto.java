36f6e3c2ad9151963d472e7f0e4f049e0fa26db8
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2024-04-29 19:49:31 +0300

    Added constraints for UserDto

--- src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/dto/UserDto.java
+++ src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/dto/UserDto.java
@@ -1,5 +1,9 @@
 package org.testtask.clearsolutions.restapi.TestTaskUsersRestApi.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,12 +13,18 @@ import java.time.LocalDate;
 @Builder
 public class UserDto {
 
+    @NotBlank(message = "Email is required and cannot be blank")
+    @Email(message = "Email must be a valid email")
     private String email;
 
+    @NotBlank(message = "First name is required and cannot be blank")
     private String firstName;
 
+    @NotBlank(message = "Last name is required and cannot be blank")
     private String lastName;
 
+    @NotNull(message = "Birth date is required and cannot be null")
+    @Past(message = "Birth date must be in the past")
     private LocalDate birthDate;
 
     private String address;
