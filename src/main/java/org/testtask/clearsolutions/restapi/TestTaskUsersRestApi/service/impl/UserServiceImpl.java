fe1f2c13d64a2e3d83ff11b11d63dd6ea11747b9
Volodymyr Romaniuk <volodyaromanyuk@gmail.com>
2024-04-29 19:49:21 +0300

    Added CRUD methods that consume id

--- src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/service/impl/UserServiceImpl.java
+++ src/main/java/org/testtask/clearsolutions/restapi/TestTaskUsersRestApi/service/impl/UserServiceImpl.java
@@ -7,17 +7,16 @@ import org.testtask.clearsolutions.restapi.TestTaskUsersRestApi.model.User;
 import org.testtask.clearsolutions.restapi.TestTaskUsersRestApi.service.UserService;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final List<User> users = new ArrayList<>();
+    private long nextId = 1;
 
     {
-        users.add(
+        createUser(
                 User.builder()
                         .email("foomail1@mail.com")
                         .firstName("John")
@@ -25,7 +24,7 @@ public class UserServiceImpl implements UserService {
                         .birthDate(LocalDate.of(2001, 1, 5))
                         .build()
         );
-        users.add(
+        createUser(
                 User.builder()
                         .email("foomail2@mail.com")
                         .firstName("Maria")
@@ -34,7 +33,7 @@ public class UserServiceImpl implements UserService {
                         .address("Aroyo Lane 3, LA")
                         .build()
         );
-        users.add(
+        createUser(
                 User.builder()
                         .email("foomail3@mail.com")
                         .firstName("Mark")
@@ -57,24 +56,31 @@ public class UserServiceImpl implements UserService {
         if (getUserByEmail(user.getEmail()).isPresent()) {
             throw new IllegalArgumentException("User with email " + user.getEmail() + " already exists");
         }
+        user.setId(nextId++);
         users.add(user);
         return user;
     }
 
     @Override
-    public Optional<User> getUserByEmail(String email) {
+    public Optional<User> findUserById(long id) {
         return users.stream()
-                .filter(user -> user.getEmail().equals(email))
+                .filter(user -> user.getId() == id)
                 .findFirst();
     }
 
     @Override
-    public User updateUserByEmail(String email, User updatedUser) {
-        User user = getUserByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException("User with email " + email + " not found"));
+    public User updateUserById(long id, User updatedUser) {
+        User user = findUserById(id)
+                .orElseThrow(() -> new UserNotFoundException("User with id " + id + " not found"));
         if (!isOlderThanMinAge(updatedUser.getBirthDate())) {
             throw new IllegalArgumentException("User must be at least " + minAge + " years old");
         }
+        if (getUserByEmail(updatedUser.getEmail()).isPresent()) {
+            if (getUserByEmail(updatedUser.getEmail()).get().getId() != id) {
+                throw new IllegalArgumentException("User with email " + user.getEmail() + " already exists");
+            }
+        }
+        user.setEmail(updatedUser.getEmail());
         user.setFirstName(updatedUser.getFirstName());
         user.setLastName(updatedUser.getLastName());
         user.setBirthDate(updatedUser.getBirthDate());
@@ -84,11 +90,54 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void deleteUserByEmail(String email) {
-        if (!users.removeIf(user -> user.getEmail().equals(email))) {
-            throw new UserNotFoundException("User with email " + email + " not found");
+    public void deleteUserById(long id) {
+        if (!users.removeIf(user -> user.getId() == id)) {
+            throw new UserNotFoundException("User with id " + id + " not found");
         }
     }
+
+
+//    @Override
+//    public User createUser(User user) {
+//        if (!isOlderThanMinAge(user.getBirthDate())) {
+//            throw new IllegalArgumentException("User must be at least " + minAge + " years old");
+//        }
+//        if (getUserByEmail(user.getEmail()).isPresent()) {
+//            throw new IllegalArgumentException("User with email " + user.getEmail() + " already exists");
+//        }
+//        user.setId(nextId++);
+//        users.add(user);
+//        return user;
+//    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        return users.stream()
+                .filter(user -> user.getEmail().equals(email))
+                .findFirst();
+    }
+
+//    @Override
+//    public User updateUserByEmail(String email, User updatedUser) {
+//        User user = getUserByEmail(email)
+//                .orElseThrow(() -> new UserNotFoundException("User with email " + email + " not found"));
+//        if (!isOlderThanMinAge(updatedUser.getBirthDate())) {
+//            throw new IllegalArgumentException("User must be at least " + minAge + " years old");
+//        }
+//        user.setFirstName(updatedUser.getFirstName());
+//        user.setLastName(updatedUser.getLastName());
+//        user.setBirthDate(updatedUser.getBirthDate());
+//        user.setAddress(updatedUser.getAddress());
+//        user.setPhoneNumber(updatedUser.getPhoneNumber());
+//        return user;
+//    }
+
+//    @Override
+//    public void deleteUserByEmail(String email) {
+//        if (!users.removeIf(user -> user.getEmail().equals(email))) {
+//            throw new UserNotFoundException("User with email " + email + " not found");
+//        }
+//    }
 
     @Override
     public List<User> getAllUsers() {
